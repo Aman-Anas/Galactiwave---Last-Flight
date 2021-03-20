15ac09f5410c7ca96530fcff977b72ca7124e746
Aman Anas <IA.AmanAnas@gmail.com>
2021-03-19 20:19:57 -0400

    fix lighting issues, add atmosphere

--- Refactor/Data/Scripts/SunCopy.py
+++ Refactor/Data/Scripts/SunCopy.py
@@ -2,7 +2,7 @@ def updateSun(cont):
     own = cont.owner
     mainSun = own.scene.objects["MainSun"]
     playerSun = own.children[0]
-    playerSun.energy = mainSun.energy
+    playerSun.energy = mainSun.energy/2
     playerSun.color = mainSun.color
     own.worldOrientation = mainSun.worldOrientation
     own.worldPosition = own.scene.objects["Player_Hitbox"].worldPosition
